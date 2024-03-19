49b0361b6561122a72aaaf2ccd3890718e8e2869
ji-loric <jirizarry@loricgames.com>
2024-03-19 13:54:51 -0700

    Conan Package Framework
    Added the necessary files to upload the built file to the Conan repository

---
+++ physx/Build/conanfile.py
@@ -0,0 +1,42 @@
+import os
+from conan import ConanFile
+from conan.tools.files import copy
+
+class sampleRecipe(ConanFile):
+    settings = "os", "build_type"
+    name = "physx"
+    version = "5.3.1"
+
+    def layout(self):
+        self.folders.build = os.path.join( ".." )
+        self.folders.source = self.folders.build
+        self.cpp.includedirs = ["include"]
+        
+        osSubDir = "win.x86_64.vc142.mt"
+        if self.settings.os == "Linux":
+            osSubDir = "linux.clang"
+            
+        buildTypeSubDir = "checked"
+        if self.settings.build_type == "Release":
+            buildTypeSubDir = "release"
+        
+        self.cpp.build.libdirs = [ os.path.join( "bin", osSubDir, buildTypeSubDir ) ]
+
+    def package(self):
+        local_include_folder = os.path.join( self.source_folder, "include" )
+        local_lib_dir = os.path.join( self.build_folder, self.cpp.build.libdirs[ 0 ] )
+        copy( self, "*.h", local_include_folder, os.path.join( self.package_folder, "include", "Physx5" ), keep_path = True )
+        copy( self, "*.lib", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
+        copy( self, "*.pdb", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
+        copy( self, "*.a", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
+
+    def package_info(self):
+        self.cpp_info.libs = [
+            "PhysX_static_64", 
+            "PhysXCommon_static_64",
+            "PhysXCooking_static_64",
+            "PhysXExtensions_static_64",
+            "PhysXFoundation_static_64"
+        ]
+        #if self.settings.os == "Linux":
+        #    [ "lib" + s for s in libList ]
