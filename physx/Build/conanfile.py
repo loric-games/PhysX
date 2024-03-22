c1edb0ebef23c693646cf479bbc4e7059c73cbe8
ji-loric <jirizarry@loricgames.com>
2024-03-22 07:52:53 -0700

    Build Script Cleanup
    Some files used on Linux had DOS endings. Cleaned those up. Also fixed the Conan file to more closely match out build system.

--- physx/Build/conanfile.py
+++ physx/Build/conanfile.py
@@ -16,16 +16,16 @@ class sampleRecipe(ConanFile):
         if self.settings.os == "Linux":
             osSubDir = "linux.clang"
             
-        buildTypeSubDir = "checked"
+        buildTypeSubDir = "debug"
         if self.settings.build_type == "Release":
-            buildTypeSubDir = "release"
+            buildTypeSubDir = "checked"
         
         self.cpp.build.libdirs = [ os.path.join( "bin", osSubDir, buildTypeSubDir ) ]
 
     def package(self):
         local_include_folder = os.path.join( self.source_folder, "include" )
         local_lib_dir = os.path.join( self.build_folder, self.cpp.build.libdirs[ 0 ] )
-        copy( self, "*.h", local_include_folder, os.path.join( self.package_folder, "include", "Physx5" ), keep_path = True )
+        copy( self, "*.h", local_include_folder, os.path.join( self.package_folder, "include" ), keep_path = True )
         copy( self, "*.lib", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
         copy( self, "*.pdb", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
         copy( self, "*.a", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
@@ -34,9 +34,11 @@ class sampleRecipe(ConanFile):
         self.cpp_info.libs = [
             "PhysX_static_64", 
             "PhysXCommon_static_64",
-            "PhysXCooking_static_64",
             "PhysXExtensions_static_64",
-            "PhysXFoundation_static_64"
+            "PhysXPvdSDK_static_64",
+            #"PhysX_static_64", <-- There is a circular dependency which must be hand edited into projects using the PhysX library on Linux
+            "PhysXFoundation_static_64",
+            "PhysXCooking_static_64"
         ]
         #if self.settings.os == "Linux":
         #    [ "lib" + s for s in libList ]
