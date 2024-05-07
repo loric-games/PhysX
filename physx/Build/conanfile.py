845fcfbcd1bb6eba7cae121478109a6524250b70
ji-loric <jirizarry@loricgames.com>
2024-05-07 07:18:02 -0700

    Adding PDB's
    There were some missing PDB's for libraries built into other libraries. These have now been added.

--- physx/Build/conanfile.py
+++ physx/Build/conanfile.py
@@ -20,7 +20,15 @@ class sampleRecipe(ConanFile):
         if self.settings.build_type == "Release":
             buildTypeSubDir = "checked"
         
-        self.cpp.build.libdirs = [ os.path.join( "bin", osSubDir, buildTypeSubDir ) ]
+        if self.settings.os == "Windows":
+            self.cpp.build.libdirs = [ 
+                os.path.join( "bin", osSubDir, buildTypeSubDir ),
+                os.path.join( "compiler", "vc16win64", "sdk_source_bin", buildTypeSubDir )
+            ]         
+        else:
+            self.cpp.build.libdirs = [ 
+                os.path.join( "bin", osSubDir, buildTypeSubDir )
+            ]        
 
     def package(self):
         local_include_folder = os.path.join( self.source_folder, "include" )
@@ -29,25 +37,30 @@ class sampleRecipe(ConanFile):
         copy( self, "*.lib", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
         copy( self, "*.pdb", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
         copy( self, "*.a", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
+        copy( self, "*.dll", local_lib_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
+        
+        if self.settings.os == "Windows":   
+            local_pdb_dir = os.path.join( self.build_folder, self.cpp.build.libdirs[ 1 ] )
+            copy( self, "*.pdb", local_pdb_dir, os.path.join( self.package_folder, "lib" ), keep_path = False )
 
     def package_info(self):
         if self.settings.os == "Linux":
             self.cpp_info.libs = [
-                "PhysX_static_64", 
+                "PhysX_static_64",
+                "PhysXCooking_static_64",
                 "PhysXCommon_static_64",
                 "PhysXExtensions_static_64",
                 "PhysXFoundation_static_64",
-                "PhysXCooking_static_64"
             ]
     
         else:
             self.cpp_info.libs = [
                 "PhysX_static_64", 
+                "PhysXCooking_static_64",
                 "PhysXCommon_static_64",
                 "PhysXExtensions_static_64",
                 "PhysXPvdSDK_static_64",
                 #"PhysX_static_64", <-- There is a circular dependency which must be hand edited into projects using the PhysX library on Linux
-                "PhysXFoundation_static_64",
-                "PhysXCooking_static_64"
+                "PhysXFoundation_static_64"
             ]
             #    [ "lib" + s for s in libList ]
